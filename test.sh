002b64c69af5d44f7aa930c673246a27a5115d79
Hudi Indrawan <79536179+Kororo212@users.noreply.github.com>
2022-10-08 15:17:08 +0000

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,20 +1,7 @@
 #!/bin/bash
-mkdir tempdir
-mkdir tempdir/templates
-mkdir tempdir/static
-cp sample_app.py tempdir/.
-cp -r templates/* tempdir/templates/.
-cp -r static/* tempdir/static/.
-echo "FROM node:alpine" >> tempdir/Dockerfile
-echo "USER root" >> tempdir/Dockerfile
-echo "RUN npm install" >> tempdir/Dockerfile
-echo "COPY ./ ./" >> tempdir/Dockerfile
-echo "COPY ./ ./" >> tempdir/Dockerfile
-echo "EXPOSE 5050" >> tempdir/Dockerfile
-echo "CMD ["npm","start"]" >> tempdir/Dockerfile
-USER root
-USER indr
-cd tempdir
+
 docker build . -t sampleapp 
 docker run -t -d -p 5050:5050 --name samplerunning sampleapp
 docker ps -a
+
+
